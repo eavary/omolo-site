2a78e3d7b306351cb009386b5c5e2b52217e0b77
Eric Avary <eavary@mac.com>
2025-01-24 15:37:05 -0800

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
+  plugins: [
+    tailwindcss(),
+  ],
+})
