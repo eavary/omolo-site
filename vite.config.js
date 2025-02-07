af40d2d70567f07a662640b6526be38217b8e650
Eric Avary <eavary@mac.com>
2025-02-06 16:27:34 -0800

    update vite build process

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
-    emptyOutDir: true
+    emptyOutDir: false
   },
   plugins: [
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'animatedConversation.js',
+          dest: ''
+        }
+      ]
+    })
   ],
   server: {
     allowedHosts: ['.ngrok-free.app'],
